2578e1e97e1ec1db136c7468f8a9f98d899dd027
wdu9 <71188956+wdu9@users.noreply.github.com>
2021-04-12 02:37:00 -0400

    Add files via upload

--- Testcode.py
+++ Testcode.py
@@ -23,6 +23,7 @@ from HARK.ConsumptionSaving.ConsIndShockModel import (
     PerfForesightConsumerType,
 )
 from HARK.ConsumptionSaving.ConsAggShockModel import CobbDouglasEconomy, AggShockConsumerType
+from HARK import MetricObject, Market, AgentType
 from scipy.optimize import brentq, minimize_scalar
 import matplotlib.pyplot as plt
 
@@ -31,35 +32,42 @@ from HARK.utilities import plot_funcs_der, plot_funcs
 
 
 
-#Steady State values to be specified in with AGent
-wage = 1
-N = 1
-tax_rate =.2
 
 #######################################
 
 class FBSNKagent(IndShockConsumerType):
     
-    #Steady State values for Wage , Labor and tax rate
-    wage = 1
-    N = 1
-    tax_rate =.2
-    
+   
     time_inv_ = IndShockConsumerType.time_inv_  + ["mu_u",
                                                    "L",
-                                                  
+                                                   "SSPmu",
+                                                   "wage",
+                                                   
                                                   ]
  
     
     def __init__(self, cycles=100, **kwds):
+        
         IndShockConsumerType.__init__(self, cycles=100, **kwds)
+        #self.wage = 1/(self.SSPmu)
+        
+        #Steady State values for Wage , Labor and tax rate
+        self.Rfree = 1.02
+        #N = self.IncUnemp*self.UnempPrb / self.wage*self.tax_rate
+        #MVMU = wage*(1-self.tax_rate)/(self.SSWmu)
+        
+    
 
     def  update_income_process(self):
+        
+        self.wage = 1/(self.SSPmu)
+        self.N = self.mu_u*(self.IncUnemp*self.UnempPrb )/ (self.wage*self.tax_rate)
+        
         PermShkDstn_U = Lognormal(np.log(self.mu_u) - (self.L*(self.PermShkStd[0])**2)/2 , self.L*self.PermShkStd[0] , 123).approx(self.PermShkCount) #Permanent Shock Distribution faced when unemployed
         PermShkDstn_E = MeanOneLogNormal( self.PermShkStd[0] , 123).approx(self.PermShkCount) #Permanent Shock Distribution faced when employed
         
         TranShkDstn_E = MeanOneLogNormal( self.TranShkStd[0],123).approx(self.TranShkCount)#Transitory Shock Distribution faced when employed
-        TranShkDstn_E.X = TranShkDstn_E.X *(1-tax_rate)*wage*N  #add wage, tax rate and labor supply
+        TranShkDstn_E.X = (TranShkDstn_E.X *(1-self.tax_rate)*self.wage*self.N)/(1-self.UnempPrb)  #add wage, tax rate and labor supply
         
         lng = len(TranShkDstn_E.X )
         TranShkDstn_U = DiscreteDistribution(np.ones(lng)/lng, self.IncUnemp*np.ones(lng)) #Transitory Shock Distribution faced when unemployed
@@ -81,11 +89,12 @@ class FBSNKagent(IndShockConsumerType):
     
     
     
+    
 IdiosyncDict={
     # Parameters shared with the perfect foresight model
     "CRRA":2.0,                           # Coefficient of relative risk aversion
-    "Rfree": 1.03,                         # Interest factor on assets
-    "DiscFac": 0.97,                       # Intertemporal discount factor
+    #"Rfree": 1.03,                         # Interest factor on assets
+    "DiscFac": 0.98,                       # Intertemporal discount factor
     "LivPrb" : [0.98],                     # Survival probability
     "PermGroFac" :[1.00],                  # Permanent income growth factor
 
@@ -100,7 +109,7 @@ IdiosyncDict={
     "UnempPrbRet" : 0.0005,                # Probability of "unemployment" while retired
     "IncUnempRet" : 0.0,                   # "Unemployment" benefits when retired
     "T_retire" : 0,                        # Period of retirement (0 --> no retirement)
-    "tax_rate" : 0.0,                      # Flat income tax rate (legacy parameter, will be removed in future)
+    "tax_rate" : 0.2,                      # Flat income tax rate (legacy parameter, will be removed in future)
 
     # Parameters for constructing the "assets above minimum" grid
     "aXtraMin" : 0.001,                    # Minimum end-of-period "assets above minimum" value
@@ -116,8 +125,8 @@ IdiosyncDict={
     "T_cycle" : 1,                         # Number of periods in the cycle for this agent type
 
     # Parameters only used in simulation
-    "AgentCount" : 10000,                  # Number of agents of this type
-    "T_sim" : 120,                         # Number of periods to simulate
+    "AgentCount" : 30000,                  # Number of agents of this type
+    "T_sim" : 200,                         # Number of periods to simulate
     "aNrmInitMean" : -6.0,                 # Mean of log initial assets
     "aNrmInitStd"  : 1.0,                  # Standard deviation of log initial assets
     "pLvlInitMean" : 0.0,                  # Mean of log initial permanent income
@@ -128,10 +137,141 @@ IdiosyncDict={
     # new parameters
      "mu_u"       : .8 ,
      "L"          : 1.3, 
+     
+    #New Economy Parameters
+     "SSWmu " : 1.1 ,                      # sequence space jacobian appendix
+     "SSPmu" :  1.2,                        # sequence space jacobian appendix
+     "calvo price stickiness":  .926,      # Auclert et al 2020
+     "calvo wage stickiness": .899,        #Auclert et al 2020
+    
 }
     
-    
+'''
+
 example = FBSNKagent(**IdiosyncDict)
 example.solve()
+example.track_vars = ['aNrm','mNrm','cNrm','pLvl']
+example.initialize_sim()
+example.simulate()
 
-plot_funcs(example.solution[0].cFunc,example.solution[0].mNrmMin,5)
+a= example.state_now['aLvl']
+c = (example.state_now['mNrm'] - example.state_now['aNrm'] )*example.state_now['pLvl']
+
+AggA = np.mean(np.array(a))
+AggC = np.mean(np.array(c))
+print(AggA)
+print(AggC)
+
+#A = .09*(1-(1/1.2))/.02
+
+
+'''
+tolerance = .001
+
+completed_loops=0
+
+go = True
+
+example = FBSNKagent(**IdiosyncDict)
+
+while go:
+    
+    example.solve()
+
+#plot_funcs(example.solution[0].cFunc,example.solution[0].mNrmMin,5)
+    example.track_vars = ['aNrm','mNrm','cNrm','pLvl']
+    example.initialize_sim()
+    example.simulate()
+
+    a = example.state_now['aLvl']
+    AggA = np.mean(np.array(a))
+    
+    
+
+    if AggA - .75 > 0 :
+        
+       example.Rfree = example.Rfree - .0001
+        
+    elif AggA-.75 < 0: 
+        example.Rfree = example.Rfree + .0001
+        
+    else:
+        break
+    
+    print(example.Rfree)
+    
+    distance = abs(AggA - .75) 
+    
+    completed_loops += 1
+    go = distance >= tolerance and completed_loops < 100
+        
+
+
+
+'''
+
+
+EconomyDict={  
+    "SSWmu " : 1.1 , # sequence space jacobian appendix
+                   "SSPmu":  1.2, # sequence space jacobian appendix
+                   " calvo price stickiness":  .926, # Auclert et al 2020
+                   "calvo wage stickiness": .899, #Auclert et al 2020
+                  
+    }
+
+
+
+class Economy(Market):
+    
+    def __init__(self, agents=None, tolerance=0.0001, act_T=12, **kwds):
+        agents = agents if agents is not None else list()
+        params = EconomyDict.copy() # this is to add eocnomy dictionary
+        
+        params["sow_vars"] = [
+            "Rfree",
+        ]
+        params.update(kwds)
+
+        Market.__init__(
+            self,
+            agents=agents,
+            reap_vars=['aLvl', 'pLvl'],
+            track_vars=[],
+            dyn_vars=["Rfree"],
+            tolerance=tolerance,
+            act_T=act_T,
+            **params
+        )
+        self.update()
+
+    def update(self):
+        
+        
+        
+    def mill_rule(self, aLvl):
+        
+        AggA = np.mean(np.array(aLvl))
+        
+        return (AggA)
+        
+        
+    def calc_dynamics(self,AggA):
+        
+        if AggA > A :
+            Rfree - .001
+            
+        else:
+            Rfree + .001
+            
+        
+        return AggDynRule(Rfree)
+    
+class AggDynRule(MetricObject):
+    
+    def __init__(self,Rfree):
+        self.Rfree = Rfree
+        self.distance_criteria = ["AFunc"]
+
+
+
+'''
