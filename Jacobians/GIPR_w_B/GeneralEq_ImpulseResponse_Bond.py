e8405d49453f74a2b5815b7c74913f66112addef
wdu9 <71188956+wdu9@users.noreply.github.com>
2021-06-24 15:40:36 -0400

    Add files via upload

--- Jacobians/GIPR_w_B/GeneralEq_ImpulseResponse_Bond.py
+++ Jacobians/GIPR_w_B/GeneralEq_ImpulseResponse_Bond.py
@@ -51,7 +51,7 @@ q_ss = N_ss * ( 1 - w_ss ) / rstar
 q_ss= 1.1855898108103429
 
 #Phillips Curves parameters
-lambda_W = .75 #probability a firm won't be able to change wage
+lambda_W = .8 #probability a firm won't be able to change wage
 lambda_P = .85  #probability a firm won't be able to change price
 
 #when lambda_W = .0000000005 and lambda_P = .85, we see labor fall , consumption and output rise, and slight price inflation.
@@ -60,8 +60,14 @@ lambda_P = .85  #probability a firm won't be able to change price
 # output still rises strongly, why does the real wage rise? because the nominal wage  rises, why does the nominal wage rise?
 # Because consumption rises inducing a rise in the MRS leading to a rise in the wage
 
+#I think the reason why the graphs flip when we set wage stickiness to be 'stickier' than price stickiness is because if nominal wages fall more it may induce the real wage to flip direction and than inverting everything
+# so for example, for a monetary policy shock,  we know for lambda_P = .85 and lambda_W = .8 will lead to plausible impulse responses for a monetary contraction
+# however if we make lambda_P = .75 and lambda_W = .8 then the results flip. What is notable is that the real wage rises now and I believe this is what is driving the change
+# Note for monetary policy shocks markups move in the same direction , WHY DOES IT FLIP??! Because Real wage?!!
 
-
+#NOTE if I raise price stickiness , than wage stickiness can be closer to price stickiness , what I mean is with lambda_P = .75 and lambda_W = .75, its the wrong direction, 
+# with lambda_P = .85 and lambda_W = .85, its the right direction, with lambda_P = .85 and lambda_W = .88 its the right direction but now the effects are very strong, (10 percent fall in real wage and consumption)
+# lambda_P = .9 and lambda_W = .93 values are the right direction and consumption falls by about 3 percent. so the higher is lambda P the better
 
 
 Lambda = ( (1 - lambda_P) / lambda_P ) * (1 - ( lambda_P / (1+rstar) ) )
@@ -76,7 +82,7 @@ phi_y = 0
 # Shock Parameters       
 Z = .01 # Initial Productivity shock
 m_e = .01 # Initial Monetary Policy Shock
-p_m=.6 # AR1 Coefficient for monetary Policy shock
+p_m=.5 # AR1 Coefficient for monetary Policy shock
 p_z =.9 # AR1 Coefficient for productivity shock
 
 
@@ -98,7 +104,7 @@ mshk = np.array(mshkList)
 #if Shk = 0 <=> productivity shock, 
 #else Shk = 1 <=> monetary policy Shock
 
-Shk = 0
+Shk = 1
 
 dZ = np.zeros((400,1))
 ShkLength = 200
@@ -212,23 +218,31 @@ for i in range(200):
     J_D_Y[i][i] = 1
         
         
-J_D_Z =  np.zeros((200,200))
-for i in range(200):
-    J_D_Z[i][i] = w_ss*N_ss/(Z_ss**2)
         
 #------------------------------------------------------------------------------
-#Stock price
 
+#Stock price     
+        
+def stksum (t):
+    
+    p = 0
+    
+    for i in range(200-t):
+        
+        p +=  - N_ss*( 1 - w_ss) / ( ( 1 + rstar)**i  * ( 1 + rstar)**2 )
+        
+    return p 
 
 J_q_r =  np.zeros((200,200))
 
+        
 for j in range(200):
     for i in range(200):
         
-        J_q_r[i][i] = - N_ss*( 1 - w_ss) / (( 1 + rstar)**2)
+        J_q_r[i][i] = stksum(i)
         
         if i < j :
-            J_q_r[i][j] = -(j-i)* N_ss*( 1 - w_ss) / (( 1 + rstar)**(j-i+1))
+            J_q_r[i][j] = stksum(i) / (1+rstar)**(j-i)
         
 
 J_q_D =  np.zeros((200,200))
@@ -236,12 +250,11 @@ J_q_D =  np.zeros((200,200))
 for j in range(200):
     for i in range(200):
         
-        J_q_D[i][i] =0 # 1 /( 1 + rstar)
+        J_q_D[i][i] = 0 # 1 /( 1 + rstar)
         
         if i < j :
             
             J_q_D[i][j] =  1 / (( 1 + rstar)**(j-i))
-            
 
 
 #----------------------------------------------------------------------------
@@ -280,17 +293,46 @@ for i in range(200):
 #------------------------------------------------------------------------------
 #Government Bonds
 
-J_B_r = np.zeros((200,200))
+
+def bondsum (t):
+    
+    p = 0
+    
+    for i in range(200-t):
+        
+        p += (tau*w_ss*N_ss - G_ss - u*mho ) / (1+rstar)**i
+        
+    return p 
+
+        
+
+#Bond Price
+
+J_qb_r = np.zeros((200,200))
+
+for i in range(200):
+    
+    J_qb_r[i][i] = - (1 / (1+rstar)**2)
+
+
+J_B_qb = np.zeros((200,200))
 
 for j in range(200):
     
     for i in range(200):
         
-        J_B_r[i][i] = 0
+        J_B_qb[i][i] = 0
         
         if i < j:
-            J_B_r[i][j] =  (tau*w_ss*N_ss - G_ss - u*mho ) * (-(j-i) /( 1+rstar)**(j-i + 1))
+            J_B_qb[i][j] =  bondsum(i) / (1+rstar)**(j-i-1)
             
+for i in range(199):
+        J_B_qb[i-1, i ] = bondsum(i)
+        
+J_B_r = np.dot(J_B_qb,J_qb_r)
+
+
+
 J_B_w = np.zeros((200,200))
 
 for j in range(200):
@@ -483,6 +525,10 @@ for i in range(199):
 
 
 #-------------------------------------------------------------------------------------
+            
+#----------------------------------------------------------------
+
+
 
 
 #------------------------------------------------------------------------------
@@ -597,12 +643,15 @@ di = np.dot(J_i_pi, dpi) + np.dot(J_i_Y, dY) + np.dot(J_i_v, dZ[200:400])
 
 
 #Dividends
-dD = np.dot(J_D_Y,dY) + np.dot(J_D_w,dw)  + np.dot(J_D_N,dN) + np.dot(J_D_Z,dZ[0:200])
+dD = np.dot(J_D_Y,dY) + np.dot(J_D_w,dw)  + np.dot(J_D_N,dN) 
 
 
 #Stock Price
 dq = np.dot(J_q_D,dD) + np.dot (J_q_r,dr)
 
+#Bond Price
+
+dq_b  = -( 1/(1+rstar)**2) *dr
 
 #Price Markup 
 dmu_p = (-1/w_ss)*dw + (1/Z_ss) * dZ[0:200]
@@ -614,8 +663,13 @@ dmu_w = (1/w_ss)*dw - ( (v/N_ss)*dN - (1/MU)*dMU)
 
 dA_o = dq  + (dB/(1+rstar) - np.dot(dr, B_ss))/(1+rstar)**2
 
-#plt.plot(dA)
-#plt.plot(dA_o)
+'''
+plt.plot(dA, label ='assets from AJAC')
+plt.plot(dA_o , label = 'assets from clearing condition')
+plt.legend()
+plt.show()
+'''
+
 #--------------------------------------------------------------------------------
 #Impulse Response Figures
 
@@ -651,7 +705,9 @@ fig.tight_layout()
 
 
 fig, axs = plt.subplots(2, 2)
-axs[0, 0].plot(100*dA/A_ss)
+axs[0, 0].plot(100*dA/A_ss, label= 'AJAC')
+axs[0, 0].plot(100*dA_o/A_ss, label =' A_clear')
+axs[0, 0].legend()
 axs[0, 0].set_title("Assets")
 axs[1, 0].plot(100*dmu_p/(1/w_ss))
 axs[1, 0].set_title("Price Markup")
@@ -746,7 +802,7 @@ axs[1, 1].plot(100*dpi[0:rangelen], 'darkgreen')
 axs[1,1].set(ylabel = '%')
 axs[1, 1].set_title("Price Inflation")
 fig.tight_layout()
-plt.savefig("GIPRZ1_flexprice.jpg", dpi=500)
+plt.savefig("GIPRM1.jpg", dpi=500)
 
 
 
@@ -764,7 +820,7 @@ axs[0, 1].set_title("Assets")
 axs[1, 1].plot(100*dN[0:rangelen]/N_ss, 'darkgreen')
 axs[1, 1].set_title("Labor")
 fig.tight_layout()
-plt.savefig("GIPRZ2_flexprice.jpg", dpi=500)
+plt.savefig("GIPRZ2.jpg", dpi=500)
 
 fig, axs = plt.subplots(2, 2)
 axs[0, 0].plot(100*dD[0:rangelen]/D_ss, 'darkgreen')
@@ -779,7 +835,7 @@ axs[0, 1].set_title("Government Bonds")
 axs[1, 1].plot(dZ[0:200], 'darkgreen')
 axs[1, 1].set_title("Z")
 fig.tight_layout()
-plt.savefig("GIPRZ3_flexprice.jpg", dpi=500)
+plt.savefig("GIPRZ3.jpg", dpi=500)
 
 
 '''
